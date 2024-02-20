d8c3b764b1fa06aeb0635e0967f5afe47e318f75
ajaymangal <ajay.mangal@dotsquares.com>
2024-02-20 19:22:59 +0530

    updated

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const http = require('http');
+const fs = require('fs');
 const { getData, setData } = require('./test');
 
 const server = http.createServer(async (req, res) => {
-    let k = "NA"
     if (req.url === '/getData') {
         getData(req, res).then(output => {
-            res.end('GetData: \n' + JSON.stringify(output, null, 2))
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(output, null, 2));
         }).catch(error => {
             console.error('Error in getData:', error);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
         });
-
+    } else if (req.url === '/') {
+        // Read the index.html file
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('404 - Not Found');
